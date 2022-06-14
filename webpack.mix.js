57b871753d0b0d918a1a6f2228a49dd528e37f25
Harbecox <harbecox@gmail.com>
2022-06-14 14:28:57 +0400

    front config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
-mix.sass('resources/js/app.sass', 'public/css')
+mix.sass('resources/sass/app.scss', 'public/css')
