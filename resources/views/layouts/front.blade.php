c589e5fedcd281c6faf1ca6e73ab05b0be9116ef
Harbecox <harbecox@gmail.com>
2022-10-03 12:49:11 +0400

    fix

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -26,10 +26,9 @@
 @if(\Request::route()->getName() == "index" || \Illuminate\Support\Facades\Route::is("show_airport"))
     <div class="bg_header @if(\Illuminate\Support\Facades\Route::is("show_airport")) airport_show @endif" style="background-image: url(@yield('header_bg','/image/bggg.png')">
         <div class="header d-flex justify-content-between align-items-center">
-            AAAAA
-{{--            <div class="logo">--}}
-{{--                <a href="{{route('index')}}"><img src="/image/logo.png"></a>--}}
-{{--            </div>--}}
+            <div class="logo">
+                <a href="{{route('index')}}"><img src="/image/logo.png"></a>
+            </div>
             <div class="header_menu">
                 <ul class="navbar">
                     <li class="nav-item"><a class="nav-link text-uppercase text-gray-1" href="{{route("all_airports")}}">Airports</a></li>
