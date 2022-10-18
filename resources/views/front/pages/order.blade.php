45076a8a3273cc5fdf0b35e73b8d5e51c64acbd4
Harbecox <harbecox@gmail.com>
2022-10-18 13:53:57 +0400

    fix

--- resources/views/front/pages/order.blade.php
+++ resources/views/front/pages/order.blade.php
@@ -9,7 +9,7 @@
             <div style="margin: 0 -100px">
                 <div class="d-flex justify-content-center new_order_btn">
                     <div class="d-flex flex-column flex-lg-row">
-                        <a href="http://127.0.0.1:8000/order" class="personal_btn d-flex align-items-center my-2 my-lg-0"><img src="/image/btn1.svg">Feel
+                        <a href="http://127.0.0.1:8000/order" class="active personal_btn d-flex align-items-center my-2 my-lg-0"><img src="/image/btn1.svg">Feel
                             the form</a>
                         <a href="mailto:Order@darkwinginflight.com" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn2.svg">By email</a>
                         <a href="https://api.whatsapp.com/send?phone=0033638888695" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn3.svg">By WhatsApp</a>
@@ -20,7 +20,7 @@
                             <input type="hidden" name="_token" value="3SdNZuSfFNza22gIstvmQANe8KzXP9dd5wA23EYO">                </form>
                         <a href="http://127.0.0.1:8000/contact" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn5.svg">Free form</a>
                     </div>
-                </div>  
+                </div>
             </div>
             <livewire:order />
         </div>
