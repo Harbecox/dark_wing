3ce293dceeb8fe79b30ba24e977af4fe23b240a2
Harbecox <harbecox@gmail.com>
2022-10-23 10:12:16 +0400

    url fix

--- resources/views/front/pages/order.blade.php
+++ resources/views/front/pages/order.blade.php
@@ -9,16 +9,16 @@
             <div style="margin: 0 -100px">
                 <div class="d-flex justify-content-center new_order_btn">
                     <div class="d-flex flex-column flex-lg-row">
-                        <a href="http://127.0.0.1:8000/order" class="active personal_btn d-flex align-items-center my-2 my-lg-0"><img src="/image/btn1.svg">Feel
+                        <a href="{{route('order')}}" class="active personal_btn d-flex align-items-center my-2 my-lg-0"><img src="/image/btn1.svg">Feel
                             the form</a>
                         <a href="mailto:Order@darkwinginflight.com" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn2.svg">By email</a>
                         <a href="https://api.whatsapp.com/send?phone=0033638888695" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn3.svg">By WhatsApp</a>
-                        <form action="http://127.0.0.1:8000/personal/upload_pdf" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('upload_pdf') }}" method="post" enctype="multipart/form-data">
                             <a onclick="this.parentNode.querySelector('input').click()" type="button" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn4.svg">Upload PDF
                             </a>
                             <input name="order_pdf" onchange="this.parentNode.submit()" type="file" style="display: none">
                             <input type="hidden" name="_token" value="3SdNZuSfFNza22gIstvmQANe8KzXP9dd5wA23EYO">                </form>
-                        <a href="http://127.0.0.1:8000/contact" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn5.svg">Free form</a>
+                        <a href="{{route('contact')}}" class="personal_btn d-flex align-items-center  my-2 my-lg-0"><img src="/image/btn5.svg">Free form</a>
                     </div>
                 </div>
             </div>
