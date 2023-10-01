f3b162d4a4a302bdd189419221b9e29fd1a2b910
harbecox <harbecox@gmail.com>
2023-10-01 19:59:25 +0400

    fix

--- resources/views/front/pages/airports.blade.php
+++ resources/views/front/pages/airports.blade.php
@@ -9,9 +9,8 @@
     <div class="airport airports">
         <div class="container">
             <div class="luxury_title d-flex flex-column align-items-center">
-                <h1 class="fw-normal lh-1 text-white text-uppercase text-center tenor">Airports</h1>
-                <p class="luxury_text fs-30 fw-normal lh-sm  text-white text-center">All the airports we work with are
-                    displayed here. For convenience,<br> use the search bar</p>
+                <h1 class="fw-normal lh-1 text-white text-uppercase text-center tenor">{{ $texts['airports']['h1'] }}</h1>
+                <p class="luxury_text fs-30 fw-normal lh-sm  text-white text-center">{{ $texts['airports']['under h1'] }}</p>
             </div>
             <livewire:map />
         </div>
