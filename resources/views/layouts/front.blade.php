3d209c298f6333b02e71ce9f0360bbab4fad8366
harbecox <harbecox@gmail.com>
2023-11-01 14:44:17 +0400

    fix

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -13,9 +13,9 @@
     <link rel="icon" type="image/x-icon" href="/favico.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/css/first.css">
-    <link async rel="stylesheet" type="text/css" href="/css/lib.css">
-    <link async rel="stylesheet" type="text/css" href="/css/style.css">
-    <link async rel="stylesheet" type="text/css" href="/css/responsive.css">
+    <link defer rel="stylesheet" type="text/css" href="/css/lib.css">
+    <link defer rel="stylesheet" type="text/css" href="/css/style.css">
+    <link defer rel="stylesheet" type="text/css" href="/css/responsive.css">
     @livewireStyles
 </head>
 <body style="background-image: url(@yield('body_bg','/image/bggg.webp'))">
