f724067d973a75ec98ca6c2f7d46c910fe66dd55
harbecox <harbecox@gmail.com>
2023-11-03 19:21:17 +0400

    fix

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -3,9 +3,9 @@
         <div class="col-xl-6 col-md-12">
             <div class="fake_input d-flex position-relative w-100 @if(strlen($name) > 0) focus @endif">
                 <div class="part left"></div>
-                <div class="part main"><span class="title">First name</span></div>
+                <div class="part main"><label for="input_first_name" class="title">First name</label></div>
                 <div class="part right"></div>
-                <input wire:model="name">
+                <input id="input_first_name" wire:model="name">
             </div>
             @error('name')
                 <span class="text-danger error">{{ $message }}</span>
@@ -14,9 +14,9 @@
         <div class="col-xl-6 col-md-12">
             <div class="fake_input d-flex position-relative w-100 @if(strlen($lname) > 0) focus @endif">
                 <div class="part left"></div>
-                <div class="part main"><span class="title">Last name</span></div>
+                <div class="part main"><label for="input_last_name" class="title">Last name</label></div>
                 <div class="part right"></div>
-                <input wire:model="lname">
+                <input id="input_last_name" wire:model="lname">
             </div>
             @error('lname')
                 <span class="text-danger error">{{ $message }}</span>
@@ -25,9 +25,9 @@
         <div class="col-xl-6 col-md-12">
             <div class="fake_input d-flex position-relative w-100 @if(strlen($email) > 0) focus @endif">
                 <div class="part left"></div>
-                <div class="part main"><span class="title">E-mail</span></div>
+                <div class="part main"><label for="input_email" class="title">E-mail</label></div>
                 <div class="part right"></div>
-                <input wire:model="email">
+                <input id="input_email" wire:model="email">
             </div>
             @error('email')
                 <span class="text-danger error">{{ $message }}</span>
@@ -36,9 +36,9 @@
         <div class="col-xl-6 col-md-12">
             <div class="fake_input d-flex position-relative w-100 @if(strlen($phone) > 0) focus @endif">
                 <div class="part left"></div>
-                <div class="part main"><span class="title">Phone number</span></div>
+                <div class="part main"><label for="input_phone" class="title">Phone number</label></div>
                 <div class="part right"></div>
-                <input wire:model="phone">
+                <input id="input_phone" wire:model="phone">
             </div>
             @error('phone')
                 <span class="text-danger error">{{ $message }}</span>
@@ -47,9 +47,9 @@
         <div class="col-md-12">
             <div class="fake_input textarea d-flex position-relative w-100 @if(strlen($question) > 0) focus @endif">
                 <div class="part left"></div>
-                <div class="part main"><span class="title">My question is about...</span></div>
+                <div class="part main"><label for="textarea_q" class="title">My question is about...</label></div>
                 <div class="part right"></div>
-                <textarea wire:model="question"></textarea>
+                <textarea id="textarea_q" wire:model="question"></textarea>
             </div>
             @error('question')
             <span class="text-danger error">{{ $message }}</span>
