636ee37bffc3ac8af635fce32272d7f9ec997f0f
Harbecox <45950499+Harbecox@users.noreply.github.com>
2024-01-12 12:04:54 +0400

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -70,7 +70,7 @@
          style="background-image: url(@yield('header_bg','/image/bggg.webp')">
         <div class="header d-flex justify-content-between align-items-center">
             <div class="logo">
-                <a href="{{route('index')}}"><img alt="darkwinginflight" src="/image/logo.png" width="216" height="116"></a>
+                <a href="{{route('index')}}"><img alt="darkwinginflight" src="/image/logo.png" width="216" height="116" style="height: 116px;"></a>
             </div>
             <div class="header_menu">
                 <ul class="navbar">
